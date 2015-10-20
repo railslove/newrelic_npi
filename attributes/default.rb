d32b34ed42992a80066b170ae8cb490e58e99add
Jakob Hilden <jakobhilden@gmail.com>
2015-10-20 10:50:27 +0200

    use license_key from newrelic cookbook if possible

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,5 @@ default['newrelic_npi']['platform'] = 'linux'
 default['newrelic_npi']['distro'] = 'debian'
 default['newrelic_npi']['user'] = ''
 default['newrelic_npi']['install_path'] = "/home/#{node['newrelic_npi']['user']}/newrelic-npi"
-default['newrelic_npi']['license_key'] = ''
+default['newrelic_npi']['license_key'] = node['newrelic']['server_monitoring']['license'] || ''
 default['newrelic_npi']['plugins'] = {}
